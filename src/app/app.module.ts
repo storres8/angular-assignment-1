cdbcc3eb5260b45557fb66ce09124c9947ab1e3d
storres8 <stevetorres7562@gmail.com>
2019-06-17 15:16:39 -0400

    created success component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { AppComponent } from "./app.component";
+import { WarningAlertComponent } from "./warning-alert/warning-alert.component";
+import { SuccessAlertComponent } from "./success-alert/success-alert.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    WarningAlertComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, WarningAlertComponent, SuccessAlertComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
